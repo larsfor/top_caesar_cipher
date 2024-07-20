d7380110959bbd1478614848c6d16739ca20762f
larsfor <21663232+larsfor@users.noreply.github.com>
2024-07-20 22:28:17 +0200

    some rubocop fixes

--- cipher.rb
+++ cipher.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 def caesar_cipher(string, rot)
   cipher = ''
   string.split('').each do |c|
     o = c.ord
-    if o.between?(65, 90)
-      cipher += ((((o + rot - 65) % 26) + 26) % 26 + 65).chr
+    cipher += if o.between?(65, 90)
+                ((((o + rot - 65) % 26) + 26) % 26 + 65).chr
 
-    elsif o.between?(97, 122)
-      cipher += ((((o + rot - 97) % 26) + 26) % 26 + 97).chr
-    
-    else
-      cipher += c
-    end
+              elsif o.between?(97, 122)
+                ((((o + rot - 97) % 26) + 26) % 26 + 97).chr
+
+              else
+                c
+              end
   end
 
   cipher
